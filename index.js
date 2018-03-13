d0b86ffba64b2665295903293546246db09b321e
rbarretto <richard.a.barretto@gmail.com>
2018-03-13 19:51:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file!
+//describe('distanceFromHqInBlocks()', function() {
+  //it('returns a distance in blocks', function() {
+    //expect(distanceFromHqInBlocks(43)).to.equal(1);
+  //})
+  function distanceFromHqInBlocks(43){
+
+  }
+  function distanceFromHqInFeet () {
+  distanceFromHqInBlocks();
+
+}
+function calculateVertical() {
+  ('()*264')
+  //takes in the beginning and
+  //destination blocks and returns the number of feet travelled
+}
+
+// calls distanceFromHqInBlocks from inside the distanceFromHqInFeet function
